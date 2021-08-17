8e0359b1655e43be3c6a2efd70775114fe489b44
akifbiyikli <akifbiyikli323@gmail.com>
2021-08-17 13:13:29 +0300

    new stuff added for exercise

--- git_exercise.py
+++ git_exercise.py
@@ -1,5 +1,24 @@
 print("hello git")
 
+git config --global user.name "username"
+git config --global user.email "email@email.com"
+
+#git init
+git init
+
+#git status
+    # 1. working directory (dossya dizine eklendiğindeki hali)
+    # 2. staging area (add)
+    # 3. git repo (commit)
+
+#git add
 
 
+#git commit
 
+#git log
+
+#git commit
+
+
+#new stuff
